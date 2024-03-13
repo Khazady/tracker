3015a8eefd8bdb73f6a794baf704e5ce70cba81d
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-13 19:53:15 +0100

    add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next/types';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
+export default function Dashboard() {
+  return <h1>dashboard page</h1>;
+}
