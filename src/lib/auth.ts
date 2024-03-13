ae5d96ec482a59092a669cbda00d72c9a24ece6d
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-13 19:56:51 +0100

    add auth.ts with Credential provider, login callback and Prisma Provider

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { getUser } from '@/lib/actions/user';
+import prisma from '@/lib/db';
+import { loginUser } from '@/lib/schemes/user.scheme';
+import { verifyPassword } from '@/lib/utils';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedCredentials = loginUser.safeParse(credentials);
+        if (validatedCredentials.success) {
+          const { email, password } = validatedCredentials.data;
+          const existingUser = await getUser(email);
+          if (!existingUser) return null;
+
+          const isValid = await verifyPassword(password, existingUser.password);
+
+          if (isValid) return existingUser;
+        }
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
