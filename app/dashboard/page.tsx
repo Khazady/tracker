a86f9e2acb2188c2ba8aef5c3007308c9d6d8b37
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-20 19:26:34 +0100

    add table and request to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { columns } from '@/components/dashboard/trendingTable/columns';
+import { DataTable } from '@/components/ui/data-table';
+import { getTrending } from '@/lib/data/market-api';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,10 +9,14 @@ export const metadata: Metadata = {
 };
 
 export default async function DashboardPage() {
+  const data = await getTrending();
   return (
     <main className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data || []} />
       </div>
     </main>
   );
