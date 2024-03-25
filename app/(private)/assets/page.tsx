dec15a52d45549302c981122304f29112b373f0f
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-25 20:20:49 +0100

    add all assets page

---
+++ app/(private)/assets/page.tsx
@@ -0,0 +1,25 @@
+import { columns } from '@/components/dashboard/trendingTable/columns';
+import { DataTable } from '@/components/ui/table/data-table';
+import { getAllCoins } from '@/lib/data/market-data/coins-api';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'All Assets',
+  description: 'Page with list of all assets',
+};
+
+export default async function AssetsPage() {
+  const data = await getAllCoins();
+  return (
+    <main className="flex-1 space-y-4 p-8 pt-6">
+      <section className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">
+          List of all Assets
+        </h2>
+      </section>
+      <section className="container mx-auto py-10">
+        <DataTable columns={columns} data={data || []} />
+      </section>
+    </main>
+  );
+}
