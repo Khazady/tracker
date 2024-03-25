e362c63c0defce2e838dcda497a8a5ba29b67b5b
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-25 14:13:54 +0100

    adjust authorized users redirect

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,15 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
+      const isOnRegister = nextUrl.pathname.startsWith('/register');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        if (isOnLogin || isOnRegister) {
+          return Response.redirect(new URL('/dashboard', nextUrl));
+        }
       }
       return true;
     },
