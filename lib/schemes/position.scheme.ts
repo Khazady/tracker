2750bf3773779e6aecfc5990dcdf1cb8c730aef1
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-04-08 12:26:57 +0200

    add Position zod scheme

---
+++ lib/schemes/position.scheme.ts
@@ -0,0 +1,33 @@
+import { Position } from '@prisma/client';
+import { z } from 'zod';
+
+export const positionScheme = z.object({
+  assetId: z.string(),
+
+  icon: z.string().url().optional(),
+  name: z.string(),
+
+  symbol: z.string(),
+  units: z.number(),
+
+  capitalInvested: z.number(),
+  buyInPrice: z.number(),
+
+  opened: z.coerce.date(),
+
+  currentPosition: z.number(),
+
+  profitLossCurrency: z.number(),
+  profitLossPercent: z.number(),
+});
+
+export const createPositionScheme = positionScheme.omit({
+  icon: true,
+  capitalInvested: true,
+  currentPosition: true,
+  profitLossCurrency: true,
+  profitLossPercent: true,
+});
+
+export type CreatePositionType = Position;
+// z.TypeOf<typeof createPositionScheme>;
